59b21a592633bd2a5f94d111dd693ccb2f590ea0
Subu Kandaswamy <b.kandaswamy@wsu.edu>
2024-02-02 09:49:49 -0800

    Feb 2 lecture

---
+++ index.js
@@ -0,0 +1,22 @@
+function appendAlert(message, type) {
+  const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    "</div>",
+  ].join("");
+  alertPlaceholder.append(wrapper);
+}
+
+function validateEmail() {
+  const email = document.getElementById("email").value;
+  //console.log(email);
+  const valid = email.includes("@");
+  if (!valid) {
+    //alert("invalid email!");
+    appendAlert("Invalid email!", "danger");
+  }
+  return valid;
+}
