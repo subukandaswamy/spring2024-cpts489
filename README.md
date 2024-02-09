d713dfd84c6268ea3161d830cd1b3b8bc8d8796b
Subu Kandaswamy <b.kandaswamy@wsu.edu>
2024-02-09 10:45:09 -0800

    Feb 9th Lecture demo code

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Spring2024 - cpts489 - Web Development
+
 Demo code from CPTS 489 - Spring 2024
 
 ## Jan 22 Monday
+
 - [x] Basic HTML & CSS
+
+## Feb 9 Friday
+
+- [x] pass by value (or) reference
+- [x] encapsulation in JavaScript
+- [x] Use of Null vs Undefines
+- [x] Autoboxing and Wrappers in JavaScript
+- [x] Expression vs Statement
+
+## Feb 14
+
+- [x] a quick tour of ECMA specification (covered a bit on Feb 9th)
--- index.js
+++ index.js
@@ -7,17 +7,20 @@ let e = null;
 //let f;
 let g = Symbol("eecs");
 
-console.log(g);
+//console.log(g);
 
 let o = {
   name: "Tim",
   dept: "eecs",
+  awards: null,
 };
+
+console.log(o.honors);
 
 let arr = [1, 2, 3];
 
-console.log(o);
-console.log(arr);
+// console.log(o);
+// console.log(arr);
 
 // function add(x, y) {
 //   return x + y;
@@ -26,3 +29,53 @@ console.log(arr);
 let add = function (x, y) {
   return x + y;
 };
+
+let num = {
+  x: 2,
+  y: 3,
+  print: function () {
+    console.log(this.x, this.y);
+  },
+};
+
+function add_object(o) {
+  o.x = 20;
+  return o.x + o.y;
+}
+
+//console.log(add_object(num));
+
+console.log(num);
+
+let n1 = 1;
+let n2 = 2;
+
+function add_primitive(x, y) {
+  return x + y;
+}
+console.log(add_primitive(n1, n2));
+
+("Subu");
+let s = new String("Subu");
+console.log(s.toUpperCase());
+
+const numObj = 12345.6789;
+
+numObj.toFixed(); // '12346'; rounding, no fractional part
+numObj.toFixed(1);
+
+add(1,2)
+
+console.log(numObj.toFixed(1));
+
+console.log((x = 10 + 15));
+
+let n = 10
+
+if (n < 5) {
+  n = 0
+} else {
+  n = 100
+}
+
+
